c656b733c9737d8edba3198fab2e8f94427d4920
Ralph <my@ralphsun.me>
2024-09-30 12:48:22 +0800

    updata: 功能完成

--- index.js
+++ index.js
@@ -1,86 +1,60 @@
 "use strict"
 
 // JSON 檔案網址
-const url = "https://shannon945.github.io/farm_produce/data.json";
-const productsList = document.querySelector(".showList");
-let data = [];
+const URL = "https://shannon945.github.io/farm_produce/data.json";
+const PRODUCTS_LIST = document.querySelector(".showList");
+const BUTTON_GROUP = document.querySelector(".button-group");
 
-/** 步驟一 **/
-function getData(){
-//打開註解，透過底下 axios.get 撈取 url 資料
-//透過 console.log 觀看是否正確撈取資料
-//將撈取回來的資料賦予在變數 data 上
+let data = []; // 初始化陣列
+getData(); // 執行取得資料 function
+
+/* Event */
+// 點擊 tpye 後執行過濾資料 function
+BUTTON_GROUP.addEventListener("click", e => {
+	if (e.target.nodeName === "BUTTON") {
+		let tab = document.querySelectorAll(".button-group button");
+		tab.forEach(i => {
+			i.classList.remove("active")
+		}); // 刪除全部 button class 裡面的 active
+		let type = e.target.dataset.type; // 取得 button 的 data-type value 
+		dataFilter(e, `${type}`); // 將 e 以及取得的 value 代入，type 為字串
+	};
+});
+
+/* fuctions */
+// 取得資料
+function getData(){ 
 	axios
-		.get(url)
+		.get(URL)
 	  .then(function (response) {
 			data = response.data;
-			renderData();
 	  });
-}
-getData();
+};
 
-function renderData() {
- /** 步驟二 **/
-//請宣告一個變數 str 並賦予值為字串型別的空字串
-//請透過 data 陣列跑 forEach ，並至少帶入第一個參數
-
+// 算繪資料
+function renderData(showData) {
 	let str = "";
 
-/** 步驟三 **/
-//以下步驟在 forEach {} 大括號內執行
-//請將以下內容賦予給 str
-//並依指示填入填空處
-// str += `<tr>
-        // <td>${請填入作物名稱}</td>
-        // <td>${請填入上價}</td>
-        // <td>${請填入中價}</td>
-        // <td>${請填入下價}</td>
-        // <td>${請填入平均價}</td>
-        // <td>${請填入交易量}</td>
-        // </tr>`;
-
-        // "上價": 39.1
-        // ​​
-        // "下價": 17.4
-        // ​​
-        // "中價": 21.1
-        // ​​
-        // "交易日期": "110.08.22"
-        // ​​
-        // "交易量": 989
-        // ​​
-        // "作物代號": "11"
-        // ​​
-        // "作物名稱": "椰子"
-        // ​​
-        // "市場代號": "104"
-        // ​​
-        // "市場名稱": "台北二"
-        // ​​
-        // "平均價": 23.9
-        // ​​
-        // "種類代碼": "N05"        
-        
-
-  data.forEach(i => {
-	 	str += `<tr>
-	           <td>${i.作物名稱}</td>
-	           <td>${i.上價}</td>
-	           <td>${i.中價}</td>
-	           <td>${i.下價}</td>
-	           <td>${i.平均價}</td>
-	           <td>${i.交易量}</td>
+	showData.forEach(i => {
+		str += `<tr>
+	          	<td>${i.作物名稱}</td>
+	            <td>${i.上價}</td>
+	            <td>${i.中價}</td>
+	            <td>${i.下價}</td>
+	            <td>${i.平均價}</td>
+	            <td>${i.交易量}</td>
 	          </tr>`;
-  })
-  
-/*＊ 步驟四 **/
-//以下步驟在 forEach {} 大括號外執行
-//請透過 innerHTML 給 productsList 變數賦予值
-//該值為變數 str
-	
-	productsList.innerHTML = str;
-	
-}
+	});
+ 	
+	PRODUCTS_LIST.innerHTML = str;	
+};
 
-/*＊ 步驟五 **/
-//請在正確的位置呼叫 renderData() (請留意非同步)
+// 過濾資料
+function dataFilter(e, type){
+	let filterDate = [];
+	filterDate = data.filter(i => {
+		return i.種類代碼 === type;
+	});
+	e.target.classList.add("active");
+	renderData(filterDate);
+};
