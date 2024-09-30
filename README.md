19d2db8d76615471c260371d039a15fd1661ffbc
Ralph <my@ralphsun.me>
2024-09-30 17:41:17 +0800

    updata: 功能完成、README

--- README.md
+++ README.md
@@ -4,12 +4,21 @@
 
 ### 0928 資料介接
 
-- 完成 [API](https://hackmd.io/@w4wBc9wkR4CvPsIeEWiLbg/S1XivuMAu/%2FQpqZIiuoQbKqGmFAYZ7oHw) 介接
+- [作業細節](https://hackmd.io/@w4wBc9wkR4CvPsIeEWiLbg/S1XivuMAu/%2FQpqZIiuoQbKqGmFAYZ7oHw)
+- 完成
 
-### 0930 資料篩選
+### 0930
 
+#### 資料篩選
+
+- [作業細節](https://hackmd.io/@w4wBc9wkR4CvPsIeEWiLbg/S1XivuMAu/%2FUDrIpNeZRz6Zax_Mt384uQ)
 - 還是卡在同樣的地方，也就是切換 tab 的時候只有點擊到的 button 會加入 active
 - 這邊的解法跟之前做 todo 的邏輯一樣，就是事先把所有的 active 都移除，然後重新加到被點擊的 button
 - 結果仔細看才發現，其實方法都一樣，先綁定 tab 的元素，然後使用 forEach 搭配 classList.remove 一次刪除
 - 只是我是把綁定放在 function 外面，而老師是放在裡面
-- 但總之這個[部分](https://hackmd.io/@w4wBc9wkR4CvPsIeEWiLbg/S1XivuMAu/%2FUDrIpNeZRz6Zax_Mt384uQ)也完成了
+- 但總之這個部分也完成了
+
+#### 資料搜尋
+
+- [作業細節](https://hackmd.io/@w4wBc9wkR4CvPsIeEWiLbg/S1XivuMAu/%2FqywznB85Q5qkhCcwyWTiGg)
+- 完成，但是一時間不知道要如何消除 input 裡面的 value，有參考之前 todo，但是不確定卡在那裡🫠
--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 const URL = "https://shannon945.github.io/farm_produce/data.json";
 const PRODUCTS_LIST = document.querySelector(".showList");
 const BUTTON_GROUP = document.querySelector(".button-group");
+const BUTTON_SEARCH = document.querySelector(".search-group");
+
 
 let data = []; // 初始化陣列
 getData(); // 執行取得資料 function
@@ -16,18 +18,40 @@ BUTTON_GROUP.addEventListener("click", e => {
 		tab.forEach(i => {
 			i.classList.remove("active")
 		}); // 刪除全部 button class 裡面的 active
-		let type = e.target.dataset.type; // 取得 button 的 data-type value 
+		let type = e.target.dataset.type; // 取得 button 的 data-type value
 		dataFilter(e, `${type}`); // 將 e 以及取得的 value 代入，type 為字串
+	};
+});
+
+// 搜尋
+BUTTON_SEARCH.addEventListener("click", e => {
+	const FILTER_INPUT = BUTTON_SEARCH.querySelector("INPUT"); 
+	if (e.target.nodeName === "BUTTON") {
+		if (FILTER_INPUT.value === "") {
+			alert("請輸入作物名稱！");
+			return;
+		} else {
+			let filterData = [];
+			filterData = data.filter(i => {
+				return i.作物名稱.match(FILTER_INPUT.value);
+			});
+			if(filterData.length === 0) {
+				PRODUCTS_LIST.innerHTML = `<tr><td colspan="6" class="text-center p-3">查詢不到交易資訊 QQ</td></tr>`;
+				return
+			}
+			renderData(filterData);
+		};
 	};
 });
 
 /* fuctions */
 // 取得資料
-function getData(){ 
+function getData(){
 	axios
 		.get(URL)
 	  .then(function (response) {
 			data = response.data;
+			renderData(data);
 	  });
 };
 
@@ -45,8 +69,8 @@ function renderData(showData) {
 	            <td>${i.交易量}</td>
 	          </tr>`;
 	});
- 	
-	PRODUCTS_LIST.innerHTML = str;	
+
+	PRODUCTS_LIST.innerHTML = str;
 };
 
 // 過濾資料
